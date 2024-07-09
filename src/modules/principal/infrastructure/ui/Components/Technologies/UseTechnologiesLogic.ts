0343acbf63f413c9163e7e964918c51152670b67
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2024-07-08 20:21:21 -0500

    master | Adding three js on project

--- src/modules/principal/infrastructure/ui/Components/Technologies/UseTechnologiesLogic.ts
+++ src/modules/principal/infrastructure/ui/Components/Technologies/UseTechnologiesLogic.ts
@@ -7,8 +7,14 @@ import type {Technology} from "../../../../models/Technology.ts";
 // Assets
 //
 import html from './utils/assets/html.webp'
-
-const PUBLIC_FIREBASE_URL = import.meta.env.PUBLIC_FIREBASE_URL
+import css from './utils/assets/css.webp'
+import docker from './utils/assets/docker.webp'
+import figma from './utils/assets/figma.webp'
+import flutter from './utils/assets/flutter.webp'
+import git from './utils/assets/git.webp'
+import javascript from './utils/assets/javascript.webp'
+import kotlin from './utils/assets/kotlin.webp'
+import mongodb from './utils/assets/mongodb.webp'
 
 export const useTechnologiesLogic = () => {
     //
@@ -18,6 +24,38 @@ export const useTechnologiesLogic = () => {
         {
             name: "HTML 5",
             icon: html
+        },
+        {
+            name: "CSS 3",
+            icon: css
+        },
+        {
+            name: "JavaScript",
+            icon: javascript
+        },
+        {
+            name: "Kotlin",
+            icon: kotlin
+        },
+        {
+            name: "Docker",
+            icon: docker
+        },
+        {
+            name: "MongoDB",
+            icon: mongodb
+        },
+        {
+            name: "Figma",
+            icon: figma
+        },
+        {
+            name: "Git",
+            icon: git
+        },
+        {
+            name: "Flutter",
+            icon: flutter
         }
     ];
 
